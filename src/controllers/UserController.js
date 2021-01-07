a3bb98971c4d0bde9e2d8e37b22ef07b7c824651
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-01-07 08:36:36 -0300

    returning background

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -142,7 +142,7 @@ module.exports = {
     const offset = (page - 1) * 9;
 
     const users = await database('users')
-      .select(['id', 'name', 'title', 'image', 'github_user'])
+      .select(['id', 'name', 'title', 'image', 'github_user', 'background'])
       .orderBy('id', 'desc')
       .limit(9)
       .offset(offset);
